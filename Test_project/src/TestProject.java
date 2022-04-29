f5fe31a26e0d22aa11348765395fec85f55805d5
Nihel212 <94166737+Nihel212@users.noreply.github.com>
2022-04-29 11:57:19 +0200

    Add files via upload

---
+++ Test_project/src/TestProject.java
@@ -0,0 +1,13 @@
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		System.out.println("xxxxxxxxxxxxxxxx");
+		String name;
+		name = "nihel";
+		name o = null;
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test-project/src/TestProject.java
@@ -0,0 +1,12 @@
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		System.out.println(System.out.getClass());
+		System.out.println(System.in.getClass());
+		System.out.println(System.err.getClass());
+		System.err.println("Hello error");
+
+	}
+
+}
